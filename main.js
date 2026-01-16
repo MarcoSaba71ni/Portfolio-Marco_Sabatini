676da39c0dc887d0e469b90831e163f28c0f6f2c
Marco Sabatini <marsab04188@stud.noroff.no>
2026-01-16 12:27:39 -0300

    updating my stack, starting show more and less logistic, running tailwind

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const contactBtn = document.getElementById('contact-btn');
+const showMoreBtn = document.getElementById("show-more-btn");
+const showLessBtn = document.getElementById("show-less-btn");
+
+
+showMoreBtn.addEventListener("click", ()=> {
+    const fullContent = document.getElementById("full-content");
+    fullContent.classList.add("block");
+
+
+
+})
